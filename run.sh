8a2d344c27d2e2d2eec07e5d0fcc6829ed5aa4d3
Aleksander Starostka <aleksander.starostka@gmail.com>
2022-03-21 14:46:25 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 rm a.out
-g++ main.cpp -std=c++17 -Wall -lpthread -I/usr/lib/x86_64-linux-gnu/
+g++ main.cpp -std=c++17 -Wall -lpthread -I/usr/lib/x86_64-linux-gnu/ -O2
 chmod +x a.out
-./a.out n=100000 threads=2 file=filenametest.txt
+./a.out n=1000000000 threads=4 file=miliard.txt
